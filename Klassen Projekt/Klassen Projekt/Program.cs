d8aa7cac07414818d6f4007b69644ea46e696553
Luca Eßlinger <luca.esslinger@mail.schwarz>
2021-12-13 08:52:17 +0100

    Test class

---
+++ Klassen Projekt/Klassen Projekt/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Linq;
+
+namespace Klassen_Projekt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kunde pers = new Kunde();
+            pers.Name = "Tim Mustermann";
+            pers.Bankkartennummer();
+            Console.ReadKey();
+        }
+    }
+
+
+    class Kunde
+    {
+        //Eigenschaften
+        public string Name { get; set; }
+        
+       
+
+        //Methode
+        public void Bankkartennummer()
+        { 
+            Console.WriteLine("DE" + "48465194651746558");
+        }
+        
+
+    }
+}
