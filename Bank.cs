a6c15161201ba7f5a1b99b4d3ba37c2a1ff76e9c
PetryE98 <eric.petry@mail.schwarz>
2021-12-31 13:12:49 +0100

    Class Bank with added Constructor

---
+++ Bank.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+public class Bank
+{
+
+	private int anzahlMitarbeiter;
+	private int anzahlFilialen;
+	private string aktuelleFiliale;
+	private int gesamtVermögen;
+	private bool isHauptfiliale;
+
+
+    public Bank(int anzahlMitarbeiter, int anzahlFilialen,string aktuelleFiliale, int gesamtVermögen, bool isHauptFiliale )
+    {
+		this.anzahlMitarbeiter = anzahlMitarbeiter;
+		this.anzahlFilialen = anzahlFilialen;
+		this.aktuelleFiliale = aktuelleFiliale;
+		this.gesamtVermögen = gesamtVermögen;
+		this.isHauptfiliale = isHauptFiliale;
+
+
+
+    }
+
+
+	public object Filialeröffnung(int Baukosten, String Standort, int Fläche;)
+    {
+		Bank BankFiliale = new Bank();
+		return 
+
+    Bank;
+
+    }
+	
+
+}
